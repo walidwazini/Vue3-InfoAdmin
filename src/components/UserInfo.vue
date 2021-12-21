8644e3418c24cbca6981cc466776bd376d49f566
Walid Wazini <wazini25@gmail.com>
2021-12-21 20:56:15 +0800

    using slot

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -1,29 +1,33 @@
 <template>
   <section>
-    <div>
-      <h3>{{ fullName }}</h3>
-      <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
-    </div>
-    <p>{{ infoText }}</p>
+    <base-card>
+      <header>
+        <h3>{{ fullName }}</h3>
+        <base-badge :type="role" :caption="role.toUpperCase()"> </base-badge>
+      </header>
+      <p>{{ infoText }}</p>
+    </base-card>
   </section>
 </template>
 
 <script>
+import BaseCard from "./BaseCard.vue";
 export default {
+  components: { BaseCard },
   props: ["fullName", "infoText", "role"],
 };
 </script>
 
 <style scoped>
-section {
+/* section {
   margin: 2rem auto;
   max-width: 30rem;
   border-radius: 12px;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   padding: 1rem;
-}
+} */
 
-section div {
+section header {
   display: flex;
   justify-content: space-between;
   align-items: center;
--- src/main.js
+++ src/main.js
@@ -2,12 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 // import TheHeader from './components/TheHeader.vue'
 import BaseBadge from './components/BaseBadge.vue';
+import BaseCard from "./components/BaseCard.vue";
 
 
 const app = createApp(App)
 
 // app.component('the-header', TheHeader);
 app.component('base-badge', BaseBadge);
-
+app.component('base-card', BaseCard)
 
 app.mount('#app')
